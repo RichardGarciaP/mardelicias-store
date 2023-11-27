145676bfb2910d40326af4c803c674292b7c8b43
RichardGarcia-AS <68579638+RichardGarciaP@users.noreply.github.com>
2023-11-27 00:42:45 -0500

    feat: add signup functionality

--- src/shared/components/titleAuth/index.tsx
+++ src/shared/components/titleAuth/index.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
-import { styles } from "./styles";
-import Typography from "@/shared/components/typography";
+import {styles} from './styles';
+import Typography from '@/shared/components/typography';
+import {TouchableOpacity, View} from 'react-native';
+import {arrowBack} from '@/shared/assets/icons';
+import Icon from '../icon';
+import {useNavigation} from '@react-navigation/native';
+import useDarkMode from '@/shared/hooks/useDarkMode';
+import {semantic} from '@/shared/constants/colors';
+import {normalize} from '@/shared/helpers';
+import {NavigationProps} from '@/shared/routes/stack';
 
 interface TitleAuthProps {
-  title: string;
+  title?: string;
+  icon?: React.ReactNode | undefined;
 }
-export default function TitleAuth({title}: TitleAuthProps) {
-
+export default function TitleAuth({title, icon}: TitleAuthProps) {
+  const {navigate, goBack} = useNavigation<NavigationProps>();
+  const {isDarkMode} = useDarkMode();
+  const stylesIcon = {
+    tintColor: isDarkMode ? semantic.background.white.w500 : semantic.text.grey,
+  };
   return (
-    <Typography style={styles.title}>{title}</Typography>
-  )
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => goBack()}>
+        <Icon
+          customStyles={{
+            ...stylesIcon,
+            width: normalize(25),
+            height: normalize(25),
+          }}
+          icon={icon ? icon : arrowBack}
+        />
+      </TouchableOpacity>
+      {title && <Typography style={styles.title}>{title}</Typography>}
+    </View>
+  );
 }
--- src/shared/helpers/services/login.js
+++ src/shared/helpers/services/login.js
@@ -1,4 +1,5 @@
-import { supabase } from "./client";
+import {USERS_TYPE_DEFINITIONS} from '@/shared/constants/global';
+import {supabase} from './client';
 
 export const signInWithEmail = async ({email, password}) => {
   return await supabase.auth.signInWithPassword({email, password});
@@ -6,4 +7,26 @@ export const signInWithEmail = async ({email, password}) => {
 
 export const signOut = async () => {
   return await supabase.auth.signOut();
+};
+
+export const createUser = async data => {
+  let newPhone = data?.phone;
+  if (data?.phone.charAt(0) === '0') {
+    newPhone = data?.phone.substring(1);
+  }
+  return await supabase.auth.admin.createUser({
+    email: data?.email,
+    password: data?.password,
+    phone: newPhone,
+    email_confirm: true,
+    user_metadata: {
+      dni: data?.dni,
+      first_name: data?.first_name,
+      last_name: data?.last_name,
+      city: data?.city,
+      direction: data?.direction,
+      direction_detail: data?.direction_detail,
+      role: USERS_TYPE_DEFINITIONS.CLIENT,
+    },
+  });
 };
