3c1e0979f4701728f960d182d0e6ab02233d7e81
RichardGarcia-AS <68579638+RichardGarciaP@users.noreply.github.com>
2023-12-03 23:56:36 -0500

    fix;margin

--- src/modules/private/cart/index.tsx
+++ src/modules/private/cart/index.tsx
@@ -52,77 +52,64 @@ export default function Cart() {
     <View style={{flex: 1}}>
       {user ? (
         <>
-          {cart?.length > 0 ? (
-            <>
-              <Wrapper>
-                <View style={{flex: 1, paddingHorizontal: normalize(24)}}>
-                  <ScrollView
-                    showsVerticalScrollIndicator={false}
-                    style={{flex: 1}}>
-                    <HeaderWithIcon icon={shoppingBag} title="cart.title" />
-                    <View style={{height: normalize(32)}} />
-                    <List
-                      between
-                      data={cart}
-                      rows={1}
-                      renderItem={renderItem}
-                    />
-                  </ScrollView>
-                </View>
-                <ButtonSheet
-                  onClose={toggleOpenDeleteItem}
-                  dispatch={openDeleteItem}>
-                  <View style={styles.bodyButtonSheet}>
-                    <Typography style={styles.titleButtonSheet}>
-                      {'cart.remove_cart'}
-                    </Typography>
-                    {selectedProductToRemove && (
-                      <View style={styles.containerProduct}>
-                        <CardProductHorizontal
-                          actions={false}
-                          product={selectedProductToRemove}
-                        />
-                      </View>
-                    )}
-
-                    <View style={styles.footerButtonSheet}>
-                      <View style={{flex: 1}}>
-                        <ButtonOutline
-                          onPress={toggleOpenDeleteItem}
-                          title="Cancelar"
-                        />
-                      </View>
-                      <View style={{width: 10}} />
-                      <View style={{flex: 1}}>
-                        <Button title="Si, remover" onPress={handleRemoveOk} />
-                      </View>
-                    </View>
-                  </View>
-                </ButtonSheet>
-              </Wrapper>
-              <View
-                style={{
-                  paddingHorizontal: normalize(24),
-                  backgroundColor: isDarkMode
-                    ? semantic.background.dark.d500
-                    : semantic.background.white.w500,
-                }}>
-                <Button onPress={() => navigate('checkout')} title="Comprar" />
-              </View>
-            </>
-          ) : (
-            <Wrapper>
-              <View
-                style={{
-                  flex: 1,
-                  paddingHorizontal: normalize(24),
-                  justifyContent: 'center',
-                }}>
-                <Typography style={{fontSize: 24, textAlign: 'center'}}>
-                  No tienes items en tu carrito
+          <Wrapper>
+            <View style={{flex: 1, paddingHorizontal: normalize(24)}}>
+              <ScrollView
+                showsVerticalScrollIndicator={false}
+                style={{flex: 1}}>
+                <HeaderWithIcon icon={shoppingBag} title="cart.title" />
+                <View style={{height: normalize(32)}} />
+                {cart && cart?.length > 0 ? (
+                  <List between data={cart} rows={1} renderItem={renderItem} />
+                ) : (
+                  <Typography style={{fontSize: 24, textAlign: 'center'}}>
+                    No tienes items en tu carrito
+                  </Typography>
+                )}
+              </ScrollView>
+            </View>
+            <ButtonSheet
+              onClose={toggleOpenDeleteItem}
+              dispatch={openDeleteItem}>
+              <View style={styles.bodyButtonSheet}>
+                <Typography style={styles.titleButtonSheet}>
+                  {'cart.remove_cart'}
                 </Typography>
+                {selectedProductToRemove && (
+                  <View style={styles.containerProduct}>
+                    <CardProductHorizontal
+                      actions={false}
+                      product={selectedProductToRemove}
+                    />
+                  </View>
+                )}
+
+                <View style={styles.footerButtonSheet}>
+                  <View style={{flex: 1}}>
+                    <ButtonOutline
+                      onPress={toggleOpenDeleteItem}
+                      title="Cancelar"
+                    />
+                  </View>
+                  <View style={{width: 10}} />
+                  <View style={{flex: 1}}>
+                    <Button title="Si, remover" onPress={handleRemoveOk} />
+                  </View>
+                </View>
               </View>
-            </Wrapper>
+            </ButtonSheet>
+          </Wrapper>
+          {cart && cart?.length > 0 && (
+            <View
+              style={{
+                paddingHorizontal: normalize(24),
+                backgroundColor: isDarkMode
+                  ? semantic.background.dark.d500
+                  : semantic.background.white.w500,
+                marginBottom: 10,
+              }}>
+              <Button onPress={() => navigate('checkout')} title="Comprar" />
+            </View>
           )}
         </>
       ) : (
